8ce9386f7ade52262bd6f2c68aefc4039c01bef7
Rob Roselius <roselius@skilldistillery.com>
2021-08-12 10:32:54 -0600

    Complete Todo REST API, pre-auth

---
+++ TodoRest/src/main/java/com/skilldistillery/todoapp/repositories/TodoRepository.java
@@ -0,0 +1,14 @@
+package com.skilldistillery.todoapp.repositories;
+
+import java.util.Set;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.skilldistillery.todoapp.entities.Todo;
+
+public interface TodoRepository extends JpaRepository<Todo, Integer>{
+	
+	Set<Todo> findByUser_Username(String username);
+	Todo findByIdAndUser_Username(int todoId, String username);
+
+}
